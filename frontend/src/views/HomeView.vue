d4bd335bab866969dbd5f767cb6505011d755718
guillaumemouchet <guillaume.mouchet@he-arc.ch>
2023-03-15 15:49:31 +0100

    ajout du logo de la he-arc

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -14,5 +14,15 @@ onMounted(() => {
 </script>
 
 <template>
-  {{ users }}
+  <div class="row justify-center">
+    <div class="col-md-4 q-ma-md">
+      <!--{{ users }}-->
+      <div class="q-body-1">
+        Le but de se projet est de pouvoir suivre ses habitudes, il est possible
+        de créer des Templates, d'y souscrire ainsi que une fois par jour
+        remplir un mini-sondage selon l'habitude qu'on veut suivre.
+      </div>
+    </div>
+  </div>
+  <img src="../assets/logo-hearc.png" class="float-right vertical-bottom" />
 </template>
